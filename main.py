0e7cedbcc71331f2fd458669a437de8b45063133
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-27 12:03:35 +0200

    Update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-
-class Blockchain
-
-class Block
+from hashlib import sha256
+from datetime import datetime
+class Block:
+  def __init__(self, data, time=datetime.today(), pre_hash='0'*64):
+    self.data = data
+    self.time = time
+    self.pre_hash = pre_hash
+    
 
 
 tkoin = Blockchain()
@@ -9,3 +13,5 @@ tkoin = Blockchain()
 tkoin.new_block(Block({'from': 'user1', 'to': 'user2', 'amount': 8}))
 tkoin.new_block(Block({'from': 'user2', 'to': 'user3', 'amount': 12}))
 tkoin.new_block(Block({'from': 'user3', 'to': 'user1', 'amount': 4}))
+
+# print(tkoin) # Prints the tkoin blocks
