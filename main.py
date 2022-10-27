4c8404f0ed4270013fe141043939f4cfc4ead132
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-27 15:02:51 +0200

    update

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ class Block:
         self.pre_hash = pre_hash
         self.nonce = 0
         self.hash = self.make_hash()
+        
+    def __str__(self):
+        text = ''
+        for row in self.__dict__.items():
+          tex += str(row[0]) + ':' + str(row[1]) + '\n'
 
     def make_hash(self):
         return sha256((str(self.time) + str(self.data) + str(self.pre_hash) + str(self.nonce)).encode()).hexdigest()
 
     def mine(self, difficulty):
-      print('Start mining:', self.data)
-      while self.hash[:difficulty] != '0' * difficulty:
-        self.nonce += 1
-        self.hash = self.make_hash()
-      print('Experiment:', self.nonce)
-      print('Mined:', self.hash, '\n')
+        print('Start mining:', self.data)
+        while self.hash[:difficulty] != '0' * difficulty:
+            self.nonce += 1
+            self.hash = self.make_hash()
+        print('Experiment:', self.nonce)
+        print('Mined:', self.hash, '\n')
 
 
 tkoin = Blockchain()
