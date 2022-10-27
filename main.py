4c8bc8290b1a695613d42c8bcfccd7c6ff2d4f45
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-27 11:43:08 +0200

    Update

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 
+class Blockchain
 
+class Block
 
 
 tkoin = Blockchain()
+
+tkoin.new_block(Block({'from': 'user1', 'to': 'user2', 'amount': 8}))
+tkoin.new_block(Block({'from': 'user2', 'to': 'user3', 'amount': 12}))
+tkoin.new_block(Block({'from': 'user3', 'to': 'user1', 'amount': 4}))
